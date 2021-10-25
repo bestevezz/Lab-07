d8194e40a1d121c990d8008ebc47178455480d9c
unknown <bryan.estevez@mail.citytech.cuny.edu>
2021-10-25 16:24:53 -0400

    Adding some more hw

---
+++ Cake.py
@@ -0,0 +1,7 @@
+cake = input("Do you want cake? (yes or no) ")
+if cake == "yes" or cake == "Yes" or cake == "y" or cake == "Y":
+    print("Great! Heres some cake!")
+elif cake == "no" or cake == "No" or cake == "n" or cake == "N":
+    print("NO CAKE FOR YOU!")
+else:
+    print("Did you even read the responses? lol")
